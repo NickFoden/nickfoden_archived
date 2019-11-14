5b2574b4f651cb4300252eba4d3b9d36d27ae309
Nick Foden <nicfoden@gmail.com>
2019-11-14 11:50:11 -0500

    Add youtube plugin and old post

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: 'gatsby-remark-embed-youtube',
+            options: {
+              width: 800,
+              height: 400,
+            },
+          },
+          {
             resolve: 'gatsby-remark-responsive-iframe',
             options: {
               wrapperStyle: 'margin-bottom: 1rem',
---
+++ src/content/stop-thinking-in-pixels.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title: 'Stop Thinking in Pixels - Keith J. Grant'
+image: img/stopThinkingInPixels.png
+author: NickFoden
+draft: false
+date: 2018-09-24T10:00:00.000Z
+tags:
+  - CSS
+---
+
+`youtube:https://www.youtube.com/embed/XanhwddQ-PM`
