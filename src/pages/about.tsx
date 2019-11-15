39b1af82d9b611d2172fa366d45c37e63038e5e8
Nick Foden <nicfoden@gmail.com>
2019-11-15 12:25:59 -0500

    Add bio to about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -36,25 +36,58 @@ const About: React.FC = () => (
 
           <PostFullContent className="post-full-content">
             <div className="post-content">
-              <p> Brooklyn, NY</p>
+              <p>
+                Born and raised in Mountain View, CA (Before google took over the town), I went to
+                html summer camps at Stanford as a kid and I have always been messing around on
+                computers. I attended college in San Francisco, and was tweaking websites for
+                friends and landing pages for different social networks. After college I moved to
+                NYC to be closer to family. I soon found a job with a design firm and absorbed more
+                and more of their IT and accounting workload, enabling the firm to let go of IT
+                contracts and consolidate the firm's backend needs.
+              </p>
+              <p>
+                In 2015, I was getting tired of consuming so many faulty websites and apps and kept
+                seeing the limitations of the app or places where I wanted to tweak the app. Some
+                times a small part of the design or other times want to add more robust features or
+                want to even totally redo the app entirely. I wanted to really start creating and
+                contributing, so I started to take MOOC's and follow various tutorials working
+                mainly with Python and Ruby, building various projects, a sandwich tracker and some
+                other small projects, but the magic was hidden behind gems and I wanted to really
+                get into the nitty gritty, end of 2016 decided to take a bigger step, kick this
+                thing into gear for real.
+              </p>
+              <p>
+                I enrolled and worked through a javascript bootcamp program where I was paired with
+                a senior developer(10+years experience) as a mentor to work on the MERN stack. After
+                completing the curriculum I have built a half dozen SPA platforms that are currently
+                in production. And joined some like minded folks as a technical cofounder of a
+                sustainabilty focused investing app which we took through Startup School at Y
+                Combinator.
+              </p>
+              <p>
+                When I'm not javascripting away or expanding my knowledge of Golang, you can find me
+                rescuing animals with Unwanted NYC Pets, and cycling or running around NYC. To be
+                honest I don't particularly enjoy writing own bios much. If you got this far I'll
+                take you out for Banh Mi or a beer or who knows, heck I'll even help you move
+                apartments if you need it. Hit me up !
+              </p>
               <br />
+              Projects:{' '}
               <a href="https://nickis.online/" target="_blank" rel="noopener noreferrer">
                 https://nickis.online/
               </a>
               <br />
+              Blogging with my Cousins:{' '}
               <a href="https://foden.dev/" target="_blank" rel="noopener noreferrer">
                 https://foden.dev/
               </a>
               <br />
-              <a href="http://about.me/NickFoden" target="_blank" rel="noopener noreferrer">
-                http://about.me/NickFoden
-              </a>
-              <br />
+              Sailing Blog:{' '}
               <a href="https://kirieelite32.com/" target="_blank" rel="noopener noreferrer">
                 https://kirieelite32.com/
               </a>
               <br />
-              <p> GET IT ON.</p>
+              <br />
             </div>
           </PostFullContent>
         </article>
